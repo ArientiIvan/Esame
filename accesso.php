98a357df96a7db9ca1025dbe41002dedb0f143a6
ArientiIvan <i.arienti@gobettivolta.gov.it>
2021-05-13 23:43:39 +0200

    backup

--- accesso.php
+++ accesso.php
@@ -16,69 +16,61 @@
             </div>
             <div class="col-sm-4"></div>
             <div class="col-sm-4">
-                <div class="card card-container">
+                <?php
+                        if(isset($_POST['enter'])){
+                            if((isset($_POST['email']) && $_POST['email'] !='') && (isset($_POST['password']) && $_POST['password'] !='')){
+                                $email = $_POST['email'];
+                                $password = $_POST['password'];
 
+                                $query = "SELECT * 
+                                        FROM utenti 
+                                        WHERE email = '". $email ."'"
+                                        ;
+                                $result = $conn->connection->query($query);
+
+                                if($result->num_rows > 0){
+
+                                    $row = mysqli_fetch_assoc($result);
+                                    
+                                    if(hash('SHA512',$password) == $row['Password']){
+
+                                        header('location:paginaPrincipale.php');
+                                        exit;
+                                    }
+                                    else{
+                                        $errore =  "Password non corretta";
+                                    }
+                                }
+                                else{
+                                    $errore =  "Email non corretta";
+                                }
+                                
+                                if(isset($errore)){
+                                    echo "<div class='error-msg'>";
+                                    echo $errore;
+                                    echo "</div>";
+                                    unset($errore);
+                                }
+                            }
+                        }
+
+                    ?>
+                <div class="card card-container">
                     <form class="form" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
                         <span id="reauth-email" class="reauth-email"></span>
-                        <input type="email" id="email" class="form-control" placeholder="Email" required autofocus>
-                        <input type="password" id="password" class="form-control" placeholder="Password" required>
+                        <input type="email" id="email" name="email" class="form-control" placeholder="Email" required autofocus>
+                        <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
                         <a href="#" class="passwordDimenticata">Password dimenticata?</a>
-                        <button class="btn btn-lg btn-primary btn-block btn-accesso" type="submit">Accedi</button>
+                        <button name= "enter" class="btn btn-primary btn-block btn-accesso" type="submit">Accedi</button>
                     </form>
                     <div class="login-oppure">
                         <hr class="hr-oppure">
                         <span class="span-oppure">Oppure</span>
                     </div>
-                    <form action="#"><button class="btn btn-lg btn-primary btn-block btn-registrazione" type="submit">Registrati</button></form>
+                    <form action="registrazione.php"><button class="btn btn-lg btn-primary btn-block btn-registrazione" type="submit">Registrati</button></form>
                 </div>
             </div>
             <div class="col-md-4">
-
-                <?php
-                    if(isset($_POST['submit'])){
-                        echo"yess";
-                        if((isset($_POST['email']) && $_POST['email'] !='') && (isset($_POST['password']) && $_POST['password'] !='')){
-
-                            $email = $_POST['email'];
-                            $password = $_POST['password'];
-
-                            $sqlEmail = "SELECT * 
-                                         FROM Utenti 
-                                         WHERE email = '". $email ."'"
-                                        ;
-                            $result = $conn->connection->query($query);
-                            
-                            if($result === TRUE){
-                                echo"primo";
-                                $row = mysqli_fetch_assoc($rs);
-                                
-                                if(password_verify($password,$row['password'])){
-                                    echo"secondo";
-                                    /*header('location:homepage.php');
-                                    exit;*/
-                                }
-                                else
-                                {
-                                    echo"secondo else";
-                                    $errore =  "Password non corretta";
-                                }
-                            }
-                            else
-                            {
-                                echo"primo else";
-                                $errore =  "Email non corretta";
-                            }
-
-                            if(isset($errore)){
-                                echo"terzo if";
-                                echo "<div class='error-msg'>";
-                                echo $errore;
-                                echo "</div>";
-                                unset($errore);
-                            }
-                        }
-                    }
-                ?>
             </div>
         </div>
     </div>
---
+++ paginaPrincipale.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<?php
+    include 'start.php';
+?>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Pagina principale</title>
+</head>
+<body style="text-align: center">
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <nav class="navbar navbar-expand-lg navbar-light" style="background: linear-gradient(to top right, #003366 3%, #0066ff 100%);">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="paginaPrincipale.php">Pagina principale</a>
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+                    <a href="accesso.php"><button class="btn btn-primary btn-block" type="submit">Disconnetti</button></a>
+                </div>
+            </nav>
+        </div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
+    
+</body>
+</html>
